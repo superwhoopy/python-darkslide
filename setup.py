c032b37ee02fd86caa99ace7b76ed8e1c0b06856
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-05-19 01:42:45 +0300

    Update setup script

--- setup.py
+++ setup.py
@@ -1,49 +1,86 @@
-from setuptools import setup, find_packages
+﻿#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+import os
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import relpath
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
 
 setup(
-    name='landslide',
-    version='1.1.3',
-    description='Lightweight markup language-based html5 slideshow generator',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
+    name='darkslide',
+    version='1.0.0',
+    license='Apache 2.0',
+    description='Lightweight markup language-based html5 slideshow generator. Forked from landslide.',
+    long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
     author='Adam Zapletal',
     author_email='adamzap@gmail.com',
-    url='http://github.com/adamzap/landslide',
-    license='Apache 2.0',
+    maintainer='Ionel Cristian Mărieș',
+    maintainer_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/python-darkslide',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
     platforms=['any'],
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Text Processing :: Markup'
+    ],
     keywords=[
         'markdown',
         'slideshow',
         'presentation',
         'rst',
         'restructuredtext',
-        'textile'
+        'textile',
+        'landslide',
     ],
-    install_requires=['Jinja2', 'Markdown', 'Pygments', 'docutils', 'six'],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Development Status :: 4 - Beta',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Multimedia :: Graphics :: Presentation',
-        'Topic :: Text Processing :: Markup'
+    install_requires=[
+        'Jinja2',
+        'Markdown',
+        'Pygments',
+        'docutils',
+        'six'
     ],
-    long_description="""\
-Landslide takes your Markdown, ReST, or Textile file(s) and generates fancy
-HTML5 slideshow like `this <http://adamzap.com/misc/presentation.html>`_.
-
-Read the `README <http://github.com/adamzap/landslide/blob/master/README.md>`_
-for formatting instructions and more information.
-""",
+    extras_require={
+        # eg: 'rst': ['docutils>=0.11'],
+    },
     entry_points={
-        "console_scripts": [
-            "landslide = landslide.main:main",
+        'console_scripts': [
+            'darkslide = darkslide.__main__:main',
         ]
     },
 )
