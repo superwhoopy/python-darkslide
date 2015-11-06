6840811bef43fbaaaf56f16eac8ebee0aac37bff
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-11-06 15:47:24 +0200

    Bump version: 2.2.0 → 2.2.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='darkslide',
-    version='2.2.0',
+    version='2.2.1',
     license='Apache 2.0',
     description='Lightweight markup language-based html5 slideshow generator. Forked from landslide.',
     long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
