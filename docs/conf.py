66aabce4ef6c8394a9e6b16b087fe14ab766546b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-10-17 04:06:05 +0300

    Bump version: 3.1.0 → 3.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Darkslide'
 year = '2015-2017'
 author = 'Ionel Cristian Mărieș'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '3.1.0'
+version = release = '3.2.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
